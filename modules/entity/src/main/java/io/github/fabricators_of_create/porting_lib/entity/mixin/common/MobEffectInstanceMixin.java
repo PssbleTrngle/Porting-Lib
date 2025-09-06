588beba5b9cb51ceabf70ede5d229d3aeb5ff1e9
Niklas Widmann <nik@somethingcatchy.net>
2025-09-06 12:50:00 +0200

    do not accss holder value directly

--- modules/entity/src/main/java/io/github/fabricators_of_create/porting_lib/entity/mixin/common/MobEffectInstanceMixin.java
+++ modules/entity/src/main/java/io/github/fabricators_of_create/porting_lib/entity/mixin/common/MobEffectInstanceMixin.java
@@ -1,5 +1,7 @@
 package io.github.fabricators_of_create.porting_lib.entity.mixin.common;
 
+import com.google.common.base.Suppliers;
+
 import com.google.common.collect.Sets;
 
 import io.github.fabricators_of_create.porting_lib.core.util.MixinHelper;
@@ -12,11 +14,9 @@ import net.minecraft.world.effect.MobEffectInstance;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Set;
+import java.util.function.Supplier;
 
 @Mixin(MobEffectInstance.class)
 public class MobEffectInstanceMixin implements MobEffectInstanceInjection {
@@ -24,17 +24,17 @@ public class MobEffectInstanceMixin implements MobEffectInstanceInjection {
 	@Final
 	private Holder<MobEffect> effect;
 
-	private final Set<EffectCure> porting_lib$cures = Sets.newIdentityHashSet();
+	private final Supplier<Set<EffectCure>> porting_lib$cures = Suppliers.memoize(() -> {
+		var set = Sets.<EffectCure>newIdentityHashSet();
+		this.effect.value().fillEffectCures(set, MixinHelper.cast(this));
+		return set;
+	});
 
 	/**
 	 * {@return the {@link EffectCure}s which can cure the {@link MobEffect} held by this {@link MobEffectInstance}}
 	 */
 	public Set<EffectCure> getCures() {
-		return porting_lib$cures;
+		return porting_lib$cures.get();
 	}
 
-	@Inject(method = "<init>(Lnet/minecraft/core/Holder;IIZZZLnet/minecraft/world/effect/MobEffectInstance;)V", at = @At("TAIL"))
-	private void addEffects(Holder<MobEffect> holder, int duration, int amplifier, boolean ambient, boolean visible, boolean showIcon, MobEffectInstance hiddenEffect, CallbackInfo ci) {
-		this.effect.value().fillEffectCures(this.porting_lib$cures, MixinHelper.cast(this));
-	}
 }
